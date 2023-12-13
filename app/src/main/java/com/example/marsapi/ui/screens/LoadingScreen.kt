66ba7d30a797fefb18ffe9d21901d4a32005e469
mohdi <mohdinzamam12125@gmail.com>
2023-12-13 11:31:16 +0530

    UI created

--- app/src/main/java/com/example/marsapi/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/marsapi/ui/screens/LoadingScreen.kt
@@ -1,8 +1,18 @@
 package com.example.marsapi.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoadingScreen() {
-    
+    Scaffold {
+        Column(modifier = Modifier.padding(it)) {
+
+        }
+    }
 }
