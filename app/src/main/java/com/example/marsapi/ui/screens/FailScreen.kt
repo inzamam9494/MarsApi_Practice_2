1e86e1ccb61b9be48a53aed87f9efb46d25be8aa
mohdi <mohdinzamam12125@gmail.com>
2023-12-18 22:38:58 +0530

    fail loading

--- app/src/main/java/com/example/marsapi/ui/screens/FailScreen.kt
+++ app/src/main/java/com/example/marsapi/ui/screens/FailScreen.kt
@@ -1,8 +1,48 @@
 package com.example.marsapi.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.marsapi.R
 
 @Composable
 fun FailLoadingScreen() {
-    
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.ic_connection_error),
+                contentDescription = "Fail to Load"
+            )
+            Text(text = "Fail to Load")
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Retry")
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun FailLoadingScreenPreview() {
+    MaterialTheme {
+        FailLoadingScreen()
+    }
 }
